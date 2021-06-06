ad41c46bb87c7a56eb244c94f116ca5889b1c8eb
teesofttech <tundeesanju@gmail.com>
2021-06-06 23:29:03 +0100

    Initial commit

---
+++ app/src/main/java/com/iqitech/ng/Affordables/AffordablesActivity.java
@@ -0,0 +1,14 @@
+package com.iqitech.ng.Affordables;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AffordablesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affordables);
+    }
+}
